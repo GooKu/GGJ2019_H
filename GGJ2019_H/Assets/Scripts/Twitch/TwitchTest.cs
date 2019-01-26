050e2d8f177559b2c8373a94acfb4265ed404e60
GooKu <kuowods@hotmail.com>
2019-01-26 13:39:20 +0800

    increase TwitchBridge prefab and Twitch IRC setting.

---
+++ GGJ2019_H/Assets/Scripts/Twitch/TwitchTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitchTest : MonoBehaviour
+{
+    public void Test(string nick, string command)
+    {
+        Debug.Log($"{nick}, {command}");
+    }
+
+    public void TestNumber(string nick, string command, int number)
+    {
+        Debug.Log($"{nick}, {command}, {number}");
+    }
+}
