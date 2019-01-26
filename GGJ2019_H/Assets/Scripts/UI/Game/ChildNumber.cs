fc6b7819d0929c9373c0a741d29043d827cb84f5
GooKu <kuowods@hotmail.com>
2019-01-26 17:49:16 +0800

    increase child number showing.

---
+++ GGJ2019_H/Assets/Scripts/UI/Game/ChildNumber.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChildNumber : MonoBehaviour
+{
+    [SerializeField]
+    private Text number;
+
+    private Transform child;
+    private Vector3 offSet = new Vector3(0, 30, 0);
+
+    public void Setting(Child child, int number)
+    {
+        this.child = child.transform;
+        this.number.text = number.ToString();
+    }
+
+    private void Update()
+    {
+        if(child != null)
+        {
+            transform.position = Camera.main.WorldToScreenPoint(child.position) + offSet;
+        }
+    }
+
+}
---
+++ GGJ2019_H/Assets/Scripts/UI/Game/ChildNumberProducter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ChildNumberProducter : MonoBehaviour
+{
+    [SerializeField]
+    private ChildNumber childNumberSample;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < CharacterManager.Instance.Children.Count; i++)
+        {
+            var childNumber = Instantiate(childNumberSample, transform);
+            childNumber.Setting(CharacterManager.Instance.Children[i], i);
+        }
+    }
+}
