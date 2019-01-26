a726ebe20a68cfdb843822c566f4454668e81d00
QiFen <QiFen.Pocket@gmail.com>
2019-01-26 13:47:47 +0800

    Add home page

---
+++ GGJ2019_H/Assets/Scripts/UI/HomePage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomePage : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Prototype");
+        Debug.Log("Start");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+}
