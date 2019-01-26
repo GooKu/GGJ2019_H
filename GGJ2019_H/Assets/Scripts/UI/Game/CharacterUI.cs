74adf073cc94225c1983798cc3cafb61fe65d8b8
GooKu <kuowods@hotmail.com>
2019-01-27 00:16:29 +0800

    increase GameStart and CharacterUI.

---
+++ GGJ2019_H/Assets/Scripts/UI/Game/CharacterUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image characterImg;
+    [SerializeField]
+    private Text characterName;
+    [SerializeField]
+    private Text hotKeyText;
+
+    public void PCInit(Sprite character, string name, string hotKey)
+    {
+        characterImg.sprite = character;
+        characterName.text = name;
+        hotKeyText.text = $"Hot Key:{hotKey}";
+    }
+}
