5bec95d49f8699fd2542089620abbe83ca303d9e
GooKu <kuowods@hotmail.com>
2019-01-26 16:43:30 +0800

    increase CountDownBar.

---
+++ GGJ2019_H/Assets/Scripts/UI/Game/CountDownBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownBar : MonoBehaviour
+{
+    [SerializeField]
+    private Timer timer;
+    [SerializeField]
+    private Text timeText;
+    [SerializeField]
+    private Image bar;
+
+    void Start()
+    {
+        timer.OnTimeChange += onTimeChange;
+    }
+
+    private void onTimeChange()
+    {
+        float value = timer.Current / 1;
+        bar.fillAmount = value;
+        timeText.text = timer.Current.ToString("0.00");
+    }
+}
