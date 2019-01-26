e131fca6dc48162474c013a2754689d4c4ff7d37
RainrainWu <s0958334772@gmail.com>
2019-01-26 22:04:57 +0800

    Add BGM.

---
+++ GGJ2019_H/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+#region Ref
+
+    [Header("Internal Ref")]
+    [SerializeField] private AudioSource _beginSource;
+    [SerializeField] private AudioSource _repeatSource;
+
+    [Header("External Ref")]
+    [SerializeField] private AudioClip _begin;
+    [SerializeField] private AudioClip _repeat;
+
+#endregion
+
+#region Monos
+
+    private void Awake(){
+
+        // Initial Ref
+        _beginSource = GetComponents<AudioSource>()[0];
+        _repeatSource = GetComponents<AudioSource>()[1];
+        _begin = Resources.Load("Audios/Begin") as AudioClip;
+        _repeat = Resources.Load("Audios/Repeat") as AudioClip;
+    }
+
+    private void Start() {
+
+        // Initial music
+        _beginSource.clip = _begin;
+        _repeatSource.clip = _repeat;
+        _repeatSource.loop = true;
+        Begin();
+        Invoke("Repeat", _begin.length);
+    }
+    
+#endregion
+
+#region Methods
+
+    private void Begin(){
+
+        _beginSource.Play();
+    }
+
+    private void Repeat(){
+
+        _repeatSource.Play();
+    }
+    
+#endregion
+
+}
